e7c08abffd88f7c5a84741951a976a094db14a93
MartineRokkedal <eaa23mrsa@students.eaaa.dk>
2024-04-18 13:20:15 +0200

    tilføjelse af java til quiz

--- js/quiz.js
+++ js/quiz.js
@@ -1,38 +1,42 @@
-// QUIZZEN STYLES HERUNDER 
-
 const questions = [
     {
-        question: "Which is the largest animal in the world",
+        question: "Hvor gammel var den ældste donor i Danmark",
         answers: [
-            {text: "Shark", correct: false},
-            {text: "Blue whale", correct: true},
-            {text: "Elephant", correct: false},
-            {text: "Giraffe", correct: false},
-        ]
+            {text: "54 år", correct: false},
+            {text: "63 år", correct: false},
+            {text: "84 år", correct: false},
+            {text: "92 år", correct: true},
+        ],
+        explanation: "Den ældste til at donere i Danmark var 92 år gammel. I 2023 var den ældste donor 84 år."
+
     }, 
     {
-        question: "Which is the smallest continent in the world",
+        question: "Hvor gammel var størstedelen af de afdøde donorer?",
         answers: [
-            {text: "Asia", correct: false},
-            {text: "Australia", correct: true},
-            {text: "Arctic", correct: false},
-            {text: "Africa", correct: false},
-        ]
+            {text: "under 40 år", correct: false},
+            {text: "40 - 50 år", correct: false},
+            {text: "50 - 60 år", correct: false},
+            {text: "Over 60 år", correct: true},
+        ],
+        explanation: "Størstedelen af donorerne var over 60 år. Kun få hundrede danskere dør hvert år på en måde, hvor organdonation bliver en mulighed."
     },
     {
-        question: "Which is the largest dessert in the world",
+        question: "Hvornår er organdonation en mulighed?",
         answers: [
-            {text: "Kalahari", correct: false},
-            {text: "Gobi", correct: false},
-            {text: "Sahara", correct: false},
-            {text: "Antarctica", correct: true},
-        ]
+            {text: "Når man dør af en stor skade i hjernen", correct: false},
+            {text: "Når man er indlagt på hospitalet på en intensivafdeling og ligger i respirator", correct: false},
+            {text: "Når individet selv eller de pårørende har sagt ja til organdonation", correct: true},
+            {text: "Når ens organer er egnede til transplantation.", correct: false},
+        ],
+        explanation: "Det er altafgørende, at der er givet samtykke til donation, før man ser på andre faktorer."
     }  
 ];
 
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
+const explanationElement = document.getElementById("explanation");
+const nextQuestionButton = document.getElementById("next-question-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
@@ -40,7 +44,7 @@ let score = 0;
 function startQuiz(){
     currentQuestionIndex = 0;
     score = 0;
-    nextButton.innerHTML = "Next";
+    nextButton.innerHTML = "Check Answer";
     showQuestion();
 }
 
@@ -48,8 +52,7 @@ function showQuestion(){
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.
-    question; 
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question; 
 
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
@@ -59,22 +62,24 @@ function showQuestion(){
         if(answer.correct){
             button.dataset.correct = answer.correct;
         }
-        button.addEventListener("click", selectAnswer);
+        button.addEventListener("click", checkAnswer);
     });
 }
 
 function resetState(){
     nextButton.style.display = "none";
+    explanationElement.style.display = "none";
+    nextQuestionButton.style.display = "none";
     while(answerButtons.firstChild){
         answerButtons.removeChild(answerButtons.firstChild);
     }
 }
 
-function selectAnswer(e){
+function checkAnswer(e){
     const selectedBtn = e.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
     if (isCorrect) {
-        selectedBtn.classList.add ("correct");
+        selectedBtn.classList.add("correct");
         score++;
     } else {
         selectedBtn.classList.add("incorrect");
@@ -83,16 +88,20 @@ function selectAnswer(e){
         if(button.dataset.correct === "true"){
             button.classList.add("correct");
         }
-        button.disabled = "true";
+        button.disabled = true;
     });
     nextButton.style.display = "block";
+    nextButton.innerHTML = "Next Question";
+    nextButton.addEventListener("click", showExplanation);
 }
 
-function showScore (){
+function showExplanation(){
     resetState();
-    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
-    nextButton.innerHTML = "Play again";
-    nextButton.style.display = "block";
+    const currentQuestion = questions[currentQuestionIndex];
+    explanationElement.innerHTML = currentQuestion.explanation;
+    explanationElement.style.display = "block";
+    nextQuestionButton.style.display = "block";
+    nextQuestionButton.addEventListener("click", handleNextButton);
 }
 
 function handleNextButton(){
@@ -104,14 +113,12 @@ function handleNextButton(){
     }
 }
 
-nextButton.addEventListener("click", () => {
-    if(currentQuestionIndex < questions.length){
-        handleNextButton();
-    } else {
-        startQuiz();
-    }
-});
+function showScore (){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play again";
+    nextButton.style.display = "block";
+    nextButton.addEventListener("click", startQuiz);
+}
 
 startQuiz();
-
-
